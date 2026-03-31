6cf91fc940c272a3ec9153dfb0c1115b9381450e
Riyad Ayoub <riyadayoub07@gmail.com>
2026-03-31 11:08:14 +0200

    Fix: Syntaxe PostgreSQL pour la table task

--- migrations/Version20260330194442.php
+++ migrations/Version20260330194442.php
@@ -20,7 +20,7 @@ final class Version20260330194442 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, is_done TINYINT NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE task (id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, is_done BOOLEAN NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
